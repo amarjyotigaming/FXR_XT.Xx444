0c19a004f7a6196d6014c409d8035bec5537fd16
amarjyotigaming <amarjyotibora285@gmail.com>
2025-02-12 00:40:29 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const global = {
+    owner: [6493353976], // GANTI PAKE ID TELE MU YA GANTENG 
+    botToken: "7572177283:AAFGIjI4HTNeC9mO8ap3dTR8yugnVUY1Ydc", // ISI BOT TOKEN LU YAK
+}
+
+module.exports = global;
+// Bocoran By Limmvz
+// https://whatsapp.com/channel/0029VawTuv09Gv7VHSUvYi37
